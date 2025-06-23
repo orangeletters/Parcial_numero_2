1c408ba438fc0515b178527a97af875deb63e141
orangeletters <orangeletters@proton.me>
2025-06-23 00:01:42 -0500

    Adding Carrito

---
+++ app/src/main/java/com/example/parcial_numero_2/Watch.java
@@ -0,0 +1,15 @@
+package com.example.parcial_numero_2;
+
+public class Watch {
+    String id;
+    String name;
+    double price;
+    int imageResId;
+
+    public Watch(String id, String name, double price, int imageResId) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.imageResId = imageResId;
+    }
+}
